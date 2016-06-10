b5e8b372a3537c8d3bf366279b6a3efe715ed5b3
Andrew Stanish <andy@youshallthrive.com>
2016-06-09 21:12:49 -0400

    setting up project for es2105/sass

--- README.md
+++ README.md
@@ -1,27 +1,17 @@
 GE GRIDS
 ========
 
-
-
 Original Aricle, read for description of funtionality:
 
 [A novel approach to mapping land conversion using Google Earth with an application to East Africa](http://www.sciencedirect.com/science/article/pii/S1364815215001747)
 
-#### README.md
-This document
-
-#### createRaster-old.js
+#### app/legacy/createRaster-old.js
 The original code base
 
-#### createRaster-maps.js
+#### app/legacy/createRaster-maps.js
 This I think is basically the original code base with maps basically dropped in to see if it would work.
 
-#### createRaster.js
+#### app/app.js
 This is the current rewrite
 
-#### gpl.txt
-Copy of the GNU Public License
-
-#### index.html
-Index file
 
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+require('core-js');
+
+module.exports = {
+    context: path.join(__dirname, 'gegrids'),
+    resolve: {
+        modulesDirectories: ["node_modules", "scripts", "stylesheets"],
+        extensions: ["", ".js", ".scss"],
+    },
+    entry: {
+        app: ["./scripts/app.js", "./stylesheets/app.scss"],
+    },
+    devtool: "source-map",
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: "bundle.js",
+        sourceMapFilename: "[file].map"
+    },
+    devServer: {
+      contentBase: ".",
+      inline: true,
+      watch: true,
+      // hot: true
+    },
+    module: {
+      loaders: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader',
+            query: {
+              optional: 'runtime',
+              nonStandard: 'false',
+            },
+          },
+          {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('css-loader?sourceMap!scss-loader?sourceMap=true&sourceMapContents=true')
+
+          }
+      ]
+    },
+    plugins: [ new ExtractTextPlugin('styles.css')]
+    // plugins: [ new webpack.HotModuleReplacementPlugin(), new ExtractTextPlugin('styles.css')]
+
+};
